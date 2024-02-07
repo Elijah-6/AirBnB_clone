7ab6a3dd0cb5bc0556dc14f9f3d6b62f2a232786
Elijah Frempong <elijahfrimpong76@gmail.com>
2024-02-07 15:14:06 +0000

    base model updated

--- models/base_model.py
+++ models/base_model.py
@@ -20,7 +20,7 @@ class BaseModel:
             save(): Updates the public instance attribute 'updated_at' with the current datetime.
             to_dict(): Returns a dictionary containing all keys/values of __dict__ of the instance.
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
             Initializes a new instance of the BaseModel class.
 
@@ -29,9 +29,20 @@ class BaseModel:
                 created_at (datetime): Current datetime when the instance is created.
                 updated_at (datetime): Current datetime when the instance is created and updated during modifications.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                else:
+                    setattr(self, key, value)
+            self.updated_at = datetime.now()
+            
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
